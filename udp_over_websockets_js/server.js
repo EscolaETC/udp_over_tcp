93404a256a27338402a134aee5c3313546618302
EscolaETC <167482337+EscolaETC@users.noreply.github.com>
2024-06-13 21:17:02 -0300

    Update server.js

--- udp_over_websockets_js/server.js
+++ udp_over_websockets_js/server.js
@@ -3,25 +3,28 @@ const express = require("express");
 const http = require("http");
 const socketio = require("socket.io");
 
-const client = datagram.createSocket("udp4");
+//& "C:\Users\alunoetc\nodejs\node.exe"
+
 const app = express();
 const server = http.createServer(app);
 const io = new socketio.Server(server);
 
 var g_socket;
 
+const clients = {}
+
 io.on("connection", (socket) => {
-    console.log("Client connected");
+    console.log("Client connected", socket.id);
+    clients[socket.id] = datagram.createSocket("udp4");
     g_socket = socket;
     socket.on("game_client", (data) => {
         //console.log("Received from game client", data);
-        client.send(data, 27015, "192.168.100.44", (error, bytes) => {});
+        clients[socket.id].send(data, 27015, "192.168.2.145", (error, bytes) => {});
     })
-});
-
-client.on("message", (data, info) => {
-    //console.log("Received from game server", data.toString());
-    g_socket.emit("game_server", data);
+    clients[socket.id].on("message", (data, info) => {
+        //console.log("Received from game server", data.toString());
+        socket.emit("game_server", data);
+    });
 });
 
 server.listen(8000, () => {
